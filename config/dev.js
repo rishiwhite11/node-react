341c0cff99d5cdadb3a5cbc15fbe4411db0ca6a2
Mayukh Basu <rishiwhite@gmail.com>
2019-03-03 15:24:04 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,3 @@
 module.exports = {
-    googleClientID: '100388953928-udvlu0jimc3pd5nik9lfrpncg4uj65la.apps.googleusercontent.com',
-    googleClientSecret:'-8DR8ZEegFukN90NRFJECGV9',
-    mongoURI:'mongodb://mayukh:apple123@ds147225.mlab.com:47225/emaily-dev',
-    cookieKey: 'adkjhkjsdhakjshdkjashdkjashdkjahksdgaskjdhakjsdhkasjhdk',
-    stripePublishableKey:'pk_test_RcPg3AQtK1hDEObRtsuLj7eM',
-    stripeSecretKey:'sk_test_hLgyOoVjyqrgACBXlZG3XHxy'
+    
 }
