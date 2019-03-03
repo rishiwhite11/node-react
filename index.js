01beb687d01344cf977da53606b04d0cbefb6bf5
rishiwhite11 <rishiwhite@gmail.com>
2019-03-03 12:36:33 +0530

    made more changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
 const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
@@ -9,7 +10,10 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
-
+app.use(bodyParser.urlencoded({
+  extended:true
+}))
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
@@ -20,7 +24,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,13 +5,18 @@ module.exports = (app) => {
         scope:['profile', 'email']
     }))
     
-    app.get('/auth/google/callback', passport.authenticate('google'));
+    app.get('/auth/google/callback', 
+    passport.authenticate('google'),
+    (req, res) =>{
+        res.redirect('/surveys');
+    });
     app.get('/api/current_user', (req, res) => {
         console.log(req.user)
+        //res.send(req.session);
         res.send(req.user)
     });
     app.get('/api/logout', (req, res) => {
         req.logout();
-        res.send(req.user);
+        res.redirect("/");
     })
 }
