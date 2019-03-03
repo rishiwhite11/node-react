308623db0d657c66828b1994cde3554f99e282d6
rishiwhite11 <rishiwhite@gmail.com>
2019-03-03 20:01:32 +0530

    merge conflict

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,16 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import reduxThunk from 'redux-thunk'
-import 'materialize-css/dist/css/materialize.min.css'
-import { Provider } from "react-redux";
-import { createStore,applyMiddleware } from "redux";
-import reducers from './reducers'
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
-ReactDOM.render(<Provider store={store}><App/></Provider>, 
-document.querySelector("#root"))
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
-console.log("STRIPE KEY IS ", process.env.REACT_APP_STRIPE_KEY);
-console.log("Environment is ", process.env.NODE_ENV);
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.querySelector('#root')
+);
